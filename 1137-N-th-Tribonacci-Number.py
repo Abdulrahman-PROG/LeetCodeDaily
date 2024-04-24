c7ab7f59a47893a03132e4a444c24e9be9fd353c
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-24 03:44:37 +0200

    [1137] [Time Beats: 97.52%] [Memory Beats: 31.32%] - LeetPush

---
+++ 1137-N-th-Tribonacci-Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        first, second, third = 0, 1, 1
+        
+        if n == 0:
+            return first
+        elif n == 1 or n == 2:
+            return second
+        
+        ans = 0
+        for i in range(3, n + 1):
+            ans = first + second + third
+            first, second, third = second, third, ans
+        
+        return ans
